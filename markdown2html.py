e17d47041eb2006b1214dfeae35b685a7a5f41c9
“JUANHercs” <juan.hercs@gmail.com>
2022-05-09 01:12:10 -0500

    spaces

--- markdown2html.py
+++ markdown2html.py
@@ -3,7 +3,8 @@
 First argument is the name of the Markdown file
 Second argument is the output file name
 Requirements:
-If the number of arguments is less than 2: print in STDERR Usage: ./markdown2html.py README.md README.html and exit 1
+If the number of arguments is less than 2: print in STDERR
+Usage: ./markdown2html.py README.md README.html and exit 1
 If the Markdown file doesnt exist: print in STDER Missing <filename> and exit 1
 Otherwise, print nothing and exit 0
 """
@@ -12,7 +13,7 @@ from os.path import exists
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print ("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html")
         exit(1)
     if not exists(sys.argv[1]):
         print("Missing {}".format(sys.argv[1]))
